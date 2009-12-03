6832900e74c3c719ecc2fc3800416ce0cd38151c
felix <felix@call-with-current-continuation.org>
2009-12-03 20:15:38 +0100

    trivial changes

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -208,7 +208,8 @@ $compile -e embedded2.scm
 ./a.out
 
 echo "======================================== timing compilation ..."
-time $compile compiler.scm -t -S -O5 -debug pb -vv
+time $compile compiler.scm -S -O5 -debug pb -vv
+time ./a.out
 
 echo "======================================== running floating-point benchmark ..."
 echo "boxed:"
