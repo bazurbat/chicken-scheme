51043ce0a13a9132d9856c2579d384931efb3606
felix <felix@call-with-current-continuation.org>
2010-07-28 14:37:22 +0200

    use irregex in chicken-status

--- chicken-status.scm
+++ chicken-status.scm
@@ -127,9 +127,8 @@ EOF
 			     (map
 			      regexp
 			      (cond ((null? pats) '(".*"))
-				    ;;XXX change for total-irregex branch:
 				    (exact (map (lambda (p)
-						  (string-append "^" (regexp-escape p) "$"))
+						  (string-append "^" (irregex-quote p) "$"))
 						pats))
 				    (else pats))))
 			    (eggs (gather-eggs patterns)))
