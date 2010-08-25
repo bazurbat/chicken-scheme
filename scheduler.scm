f09a99fe2238b497797a6e53654bb89cb95ce3aa
felix <felix@y.(none)>
2010-08-25 19:24:48 +0200

    Revert "reverted incorrect change"
    This reverts commit fb102e7a111b9d3b48d6d32b0ad3f5f2b556c62b.

--- scheduler.scm
+++ scheduler.scm
@@ -355,6 +355,8 @@ EOF
 
 (define ##sys#fdset-clear
   (foreign-lambda* void ([int fd])
+    "FD_CLR(fd, &C_fdset_input);"
+    "FD_CLR(fd, &C_fdset_input);"
     "FD_CLR(fd, &C_fdset_input_2);"
     "FD_CLR(fd, &C_fdset_output_2);") )
 
