b2ccac5a895ed37cbc44685aec5138e5e060387b
felix <felix@call-with-current-continuation.org>
2011-01-02 11:48:50 +0100

    unsigned-integer64 return type

--- c-backend.scm
+++ c-backend.scm
@@ -1330,7 +1330,7 @@
        (sprintf "C_mpointer(&~a,(void*)" dest) )
       ((c-pointer) (sprintf "C_mpointer_or_false(&~a,(void*)" dest))
       ((integer integer32) (sprintf "C_int_to_num(&~a," dest))
-      ((integer64) (sprintf "C_a_double_to_num(&~a," dest))
+      ((integer64 unsigned-integer64) (sprintf "C_a_double_to_num(&~a," dest))
       ((size_t) (sprintf "C_int_to_num(&~a,(int)" dest))
       ((unsigned-integer unsigned-integer32) (sprintf "C_unsigned_int_to_num(&~a," dest))
       ((long) (sprintf "C_long_to_num(&~a," dest))
