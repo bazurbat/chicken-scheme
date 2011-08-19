0f53037b1d95eb321131ad47a046db5e4835db81
felix <felix@call-with-current-continuation.org>
2011-08-19 21:26:30 +0200

    types.db work; fixes; tests; fixes; tests...

--- tests/typematch-tests.scm
+++ tests/typematch-tests.scm
@@ -1,20 +1,24 @@
 ;;;; typematch-tests.scm
 
 
+(use lolevel)
+
+
 (define-syntax check
   (syntax-rules ()
     ((_ x not-x t)
      (begin
+       (print "check " 't " " 'x)
        (compiler-typecase x
 	 (t 'ok))
        (compiler-typecase not-x
-	 ((not t) 'ok))
-       (ms t x not-x)))))
+	 ((not t) 'ok))))))
 
 (define-syntax checkp
   (syntax-rules ()
     ((_ p x t)
      (let ((tmp x))
+       (print "check predicate " 't " " 'p)
        (if (p tmp)
 	   (compiler-typecase tmp
 	     (t 'ok)))
@@ -31,14 +35,14 @@
 	   (foo1 (gensym 'foo1))
 	   (foo2 (gensym 'foo2)))
        `(begin
-	  (print t1 " = " t2)
+	  (print ',t1 " = " ',t2)
 	  (: ,foo1 (-> ,t1))
 	  (: ,foo2 (-> ,t2))
 	  (define (,foo1) (bar))
 	  (define (,foo2) (bar))
 	  (compiler-typecase (,foo1)
 	    (,t2 'ok))
-	  (print t2 " = " t1)
+	  (print ',t2 " = " ',t1)
 	  (compiler-typecase (,foo2)
 	    (,t1 'ok)))))))
 
@@ -50,7 +54,7 @@
 	   (foo1 (gensym 'foo1))
 	   (foo2 (gensym 'foo2)))
        `(begin
-	  (print t1 " != " t2)
+	  (print ',t1 " != " ',t2)
 	  (: ,foo1 (-> ,t1))
 	  (: ,foo2 (-> ,t2))
 	  (define (,foo1) (bar))
@@ -58,7 +62,7 @@
 	  (compiler-typecase (,foo1)
 	    (,t2 (bomb))
 	    (else 'ok))
-	  (print t2 " != " t1)
+	  (print ',t2 " != " ',t1)
 	  (compiler-typecase (,foo2)
 	    (,t1 (bomb))
 	    (else 'ok)))))))
@@ -68,21 +72,21 @@
    (lambda (x r c)
      (let ((fname (gensym))
 	   (fname2 (gensym))
-	   (type (cadr x))
-	   (val (caddr x))
-	   (nval (cadddr x)))
+	   (val (cadr x))
+	   (nval (caddr x))
+	   (type (cadddr x)))
        `(begin
-	  (print "specialize " type)
+	  (print "specialize " ',type)
 	  (: ,fname (,type -> *)
 	     ((,type) 'ok)
 	     (((not ,type)) 'ok-too))
 	  (define (,fname x) (bomb))
-	  (assert (eq? 'ok (,fname ,val)))
-	  (assert (eq? 'ok-too (,fname ,nval)))
+	  (assert (eq? 'ok (,fname ,val)) "did not specialize" ',val ',type)
+	  (assert (eq? 'ok-too (,fname ,nval)) "did specialize" ',val ',type)
 	  (: ,fname2 (* -> *)
 	     (((not ,type)) (bomb)))
 	  (define (,fname2 x) 'ok)
-	  (print "specialize not " type)
+	  (print "specialize not " ',type)
 	  (,fname2 ,val))))))
 
 
@@ -108,11 +112,31 @@
 (check (make-blob 10) 1.2 blob)
 (check (address->pointer 0) 1.2 pointer)
 (check (make-pointer-vector 1) 1.2 pointer-vector)
-(check (make-locative 'a) 1.2 locative)
+(check (make-locative "a") 1.2 locative)
 (check (##sys#make-structure 'promise) 1 (struct promise))
 (check '(1 . 2.3) '(a) (pair fixnum float))
 (check '#(a) 1 (vector symbol))
 (check '("ok") 1 (list string))
+
+(ms 123 1.2 fixnum)
+(ms "abc" 1.2 string)
+(ms 'abc 1.2 symbol)
+(ms #\x 1.2 char)
+(ms #t 1.2 boolean)
+(ms '(1) 1.2 pair)
+(ms '(1 . 2) '() pair)
+(ms + 1.2 procedure)
+(ms '#(1) 1.2 vector)
+(ms '() 1 null)
+(ms (void) 1.2 undefined)
+(ms (current-input-port) 1.2 port)
+(ms (make-blob 10) 1.2 blob)
+(ms (address->pointer 0) 1.2 pointer)
+(ms (make-pointer-vector 1) 1.2 pointer-vector)
+(ms (make-locative "a") 1.2 locative)
+(ms (##sys#make-structure 'promise) 1 (struct promise))
+(ms '(1 . 2.3) '(a) (pair fixnum float))
+(ms '#(a) 1 (vector symbol))
 
 (checkp boolean? #t boolean)
 (checkp boolean? #f boolean)
@@ -141,7 +165,6 @@
 (m number fixnum)
 (m number float)
 (m list null)
-(mn list pair)
 (m pair (pair number string))
 (m procedure (procedure () *))
 (mn (procedure (*) *) (procedure () *))
