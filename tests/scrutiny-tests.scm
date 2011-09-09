087ee9c2ee8f586ef865cfda95ea491b2370d88f
felix <felix@call-with-current-continuation.org>
2011-09-09 16:48:22 +0200

    resurrected enforcement test for user-decls

--- tests/scrutiny-tests.scm
+++ tests/scrutiny-tests.scm
@@ -88,6 +88,21 @@
 (when (foo7 x)
   (+ x 1))				; will warn about "x" being a string
 
+;; declared procedure types are enforcing
+(define-type s2s (string -> symbol))
+
+(: foo8 s2s)
+(define (foo8 x) (string->symbol x))
+(: foo9 s2s)
+(declare (enforce-argument-types foo9))
+(define (foo9 x) (string->symbol x))
+
+(define (foo10 x)
+  (foo8 x)
+  (+ x 1)			; foo8 does not enforce x (no warning)
+  (foo9 x)			; + enforces number on x
+  (+ x 1))			; foo9 does enforce
+
 ;; trigger warnings for incompatible types in "the" forms
 (define (foo10 x)
   (string-append (the pair (substring x 0 10))) ; 1
