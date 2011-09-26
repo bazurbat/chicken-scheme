017918bd4b374b4743fe6a4009a5d16532047885
felix <felix@call-with-current-continuation.org>
2011-09-26 09:11:58 +0200

    - simplify use of "over-all-instantiations" - combining instantiations over union-types uses fallback "*" type only in exact mode - fixed broken Node->Sexpr transformation for "##core#typecase" without "else"-clause - added more test-cases for type-matching
    Squashed commit of the following:
    
    commit 56299cdc71ccbc6342b4614014536b715ff3747c
    Author: felix <felix@call-with-current-continuation.org>
    Date:   Mon Sep 26 08:34:10 2011 +0200
    
        added some testcases
    
    commit 2ab58471a67b474197714aeb98a17a44b6ca8416
    Author: felix <felix@call-with-current-continuation.org>
    Date:   Sun Sep 25 15:26:04 2011 +0200
    
        simplified o-a-i, fallback to * for unbound typevars only in exact mode, fixed bug in build-expression-tree for typecase

--- tests/typematch-tests.scm
+++ tests/typematch-tests.scm
@@ -1,7 +1,7 @@
 ;;;; typematch-tests.scm
 
 
-(use lolevel)
+(use lolevel data-structures)
 
 
 (define-syntax check
@@ -211,3 +211,28 @@
 	(symbol 's)
 	(fixnum 'f)
 	((or fixnum symbol) 'sf))))
+
+(: f3 (forall (a) ((list-of a) -> a)))
+(define f3 car)
+(define xxx '(1))
+
+(compiler-typecase (f3 (the (or (vector-of fixnum) (list-of fixnum)) xxx))
+  (fixnum 'ok))
+
+(assert
+ (eq? 'ok
+      (compiler-typecase (list 123)
+	((forall (a) (or (vector-of a) (list-of a))) 'ok)
+	(else 'not-ok))))
+
+(: f4 (forall (a) ((or fixnum (list-of a)) -> a)))
+(define f4 identity)
+
+(compiler-typecase (f4 '(1))
+  (fixnum 'ok))
+
+(assert
+ (eq? 'ok (compiler-typecase (f4 1)
+	    (fixnum 'not-ok)
+	    (else 'ok))))
+
