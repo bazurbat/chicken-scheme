a9bd42146f2debc6f651123488d7e6a870ce3ab0
Peter Bex <peter.bex@xs4all.nl>
2012-06-24 18:07:51 +0200

    Add finalizer test, which was accidentally left out of commit 8a6d9a46185f4ae0498792840a5ebda659eaed61

---
+++ tests/finalizer-error-test.scm
@@ -0,0 +1,17 @@
+;;;; finalizer-error-test.scm - by "megane"
+
+(define n 10000)
+
+(define (make-objects n)
+  (let loop [(i 0)]
+    (let [(o (make-vector 100))]
+      ;(print "making " i)
+      (set-finalizer! o (lambda (ob) (print* " " i)))
+      (if (< i n)
+	  (loop (+ 1 i))))))
+
+(set-finalizer! (make-vector 100) (lambda (ob) (+ i 'a)))
+
+(make-objects n)
+
+(print "done")
