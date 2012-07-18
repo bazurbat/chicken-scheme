5a0e7eb5fd2147189304b2ab37a36c8e05642485
Peter Bex <peter.bex@xs4all.nl>
2012-07-18 20:26:27 +0200

    Fix hang in irregex-fold caused by patterns matching the empty string (upstream changeset ba70feace1dd)
    Signed-off-by: Christian Kellermann <ckeen@pestilenz.org>

--- tests/test-irregex.scm
+++ tests/test-irregex.scm
@@ -358,6 +358,11 @@
        rope-chunker
        (rope "bob@test.com and fred@example.com")
        (lambda (src i s) (reverse s))))
+  (test-equal '("poo poo ")
+      (irregex-fold '(* "poo ")
+                    (lambda (i m s) (cons (irregex-match-substring m) s))
+                    '()
+                    "poo poo platter"))
   )
 
 
