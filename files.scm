35367c581c590eb9021234babf74428027287478
Evan Hanson <evhan@foldling.org>
2013-06-01 16:18:30 +1200

    Strip all trailing slashes from directory pathname parts
    This causes decompose-pathname (and its derivatives pathname-directory,
    pathname-replace-file, etc.) to strip all trailing slashes from the
    directory parts of pathnames, rather than just the last one.
    
    Signed-off-by: Peter Bex <peter.bex@xs4all.nl>
    Signed-off-by: Christian Kellermann <ckeen@pestilenz.org>

--- files.scm
+++ files.scm
@@ -161,11 +161,13 @@ EOF
 
 (define (chop-pds str)
   (and str
-       (let ((len (##sys#size str)))
-	 (if (and (fx>= len 1)
-		  (*char-pds? (##core#inline "C_subchar" str (fx- len 1)) ) )
-	     (##sys#substring str 0 (fx- len 1))
-	     str) ) ) )
+       (let lp ((len (##sys#size str)))
+	 (cond ((and (fx>= len 1)
+		     (*char-pds? (##core#inline "C_subchar" str (fx- len 1))))
+		(lp (fx- len 1)))
+	       ((fx< len (##sys#size str))
+		(##sys#substring str 0 len))
+	       (else str)))))
 
 (define make-pathname)
 (define make-absolute-pathname)
@@ -231,9 +233,10 @@ EOF
 	 [strip-pds
 	  (lambda (dir)
 	    (and dir
-		 (if (member dir '("/" "\\"))
-		     dir
-		     (chop-pds dir) ) ) )] )
+	         (let ((chopped (chop-pds dir)))
+		   (if (fx> (##sys#size chopped) 0)
+		       chopped
+		       (##sys#substring dir 0 1) ) ) ) )] )
     (lambda (pn)
       (##sys#check-string pn 'decompose-pathname)
       (if (fx= 0 (##sys#size pn))
