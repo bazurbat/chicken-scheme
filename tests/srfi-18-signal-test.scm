5d2b4fadd0913c03b6fd457fd38d63f501f48fc8
Christian Kellermann <ckeen@pestilenz.org>
2013-12-12 05:29:13 -0500

    Fix #1081 by explicitly setting the interval to 0 in setitimer.

--- tests/srfi-18-signal-test.scm
+++ tests/srfi-18-signal-test.scm
@@ -29,6 +29,8 @@
    "struct itimerval timer;"
    "timer.it_value.tv_sec = 1;"
    "timer.it_value.tv_usec = 0;"
+   "timer.it_interval.tv_sec = 0;"
+   "timer.it_interval.tv_usec = 0;"
    "setitimer(ITIMER_REAL, &timer, NULL);\n"
    "#endif"))
 
