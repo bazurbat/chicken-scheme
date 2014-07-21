872bb1e277600257f589323b6ac393ec9a59272f
Oleg Kolosov <bazurbat@gmail.com>
2014-07-22 00:10:08 +0400

    Added Windows _mkdir alias for files module

--- files.scm
+++ files.scm
@@ -46,6 +46,11 @@
 #ifndef _WIN32
 # include <sys/stat.h>
 # define C_mkdir(str)       C_fix(mkdir(C_c_string(str), S_IRWXU | S_IRWXG | S_IRWXO))
+#elif _MSC_VER
+# ifdef HAVE_DIRECT_H
+#  include <direct.h>
+# endif
+# define C_mkdir(str)       C_fix(_mkdir(C_c_string(str)))
 #else
 # define C_mkdir(str)	    C_fix(mkdir(C_c_string(str)))
 #endif
