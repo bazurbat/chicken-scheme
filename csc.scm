bazurbat/chicken-scheme

commit be5b0827ad02473c7fbf277b8153dabb8863e973

fix static compilation in cygwin
Signed-off-by: Peter Bex <peter.bex@xs4all.nl>
Signed-off-by: felix <felix@call-with-current-continuation.org>

csc.scm
@@ -119,10 +119,7 @@
 (define windows-shell WINDOWS_SHELL)
 (define generate-manifest #f)
 
-(define libchicken
-  (if cygwin
-      (string-append "cyg" INSTALL_LIB_NAME "-0")
-      (string-append "lib" INSTALL_LIB_NAME)))
+(define libchicken (string-append "lib" INSTALL_LIB_NAME))
 
 (define default-library
   (string-append libchicken "." library-extension))

commit 870c3538c04b3c6294bf850234c32e29eeb030fa

Fix bug in deploy mode under Cygwin introduced by be5b0827ad02473c7fbf277b8153dabb8863e973
Signed-off-by: Evan Hanson <evhan@foldling.org>

csc.scm
@@ -120,6 +120,10 @@
 (define generate-manifest #f)
 
 (define libchicken (string-append "lib" INSTALL_LIB_NAME))
+(define dynamic-libchicken
+  (if cygwin
+      (string-append "cyg" INSTALL_LIB_NAME "-0")
+      libchicken))
 
 (define default-library
   (string-append libchicken "." library-extension))
@@ -974,7 +978,7 @@ EOF
 (define (copy-libraries targetdir)
   (let ((lib (make-pathname
 	      (target-lib-path) 
-	      libchicken
+	      dynamic-libchicken
 	      (cond (osx "dylib")
 		    ((or mingw cygwin) "dll")
 		    (else (string-append