ac1b3affa672b0f726638bc73713f18a85a8447e
Oleg Kolosov <bazurbat@gmail.com>
2014-10-26 00:55:30 +0400

    Removed manual and misc files

--- .dir-locals.el
+++
@@ -1,4 +0,0 @@
-((nil . ((tab-width . 8)))
- (scheme-mode . ((indent-tabs-mode . t)))
- (lisp-mode . ((indent-tabs-mode . t)))
- (c-mode . ((indent-tabs-mode . nil))))
---
+++ README.md
@@ -0,0 +1,2 @@
+CHICKEN Scheme
+=====
