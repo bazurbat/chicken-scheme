7da33f179feb8bfe6ed8b7251c15ecea78d76a35
Oleg Kolosov <bazurbat@gmail.com>
2014-10-26 00:13:39 +0400

    Moved foreign code from posixunix.scm to separate file

---
+++ posix-unix.h
@@ -0,0 +1,333 @@
+#ifndef _POSIX_UNIX_H_
+#define _POSIX_UNIX_H_
+
+static C_TLS int C_wait_status;
+
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <sys/utsname.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <pwd.h>
+#include <utime.h>
+
+#ifdef HAVE_GRP_H
+#include <grp.h>
+#endif
+
+#include <sys/mman.h>
+#include <sys/poll.h>
+
+#ifndef O_FSYNC
+# define O_FSYNC O_SYNC
+#endif
+
+#ifndef PIPE_BUF
+# define PIPE_BUF 1024
+#endif
+
+#ifndef O_BINARY
+# define O_BINARY        0
+#endif
+#ifndef O_TEXT
+# define O_TEXT          0
+#endif
+
+#ifndef ARG_MAX
+# define ARG_MAX 256
+#endif
+
+#ifndef MAP_FILE
+# define MAP_FILE    0
+#endif
+
+#ifndef MAP_ANON
+# define MAP_ANON    0
+#endif
+
+#if defined(HAVE_CRT_EXTERNS_H)
+# include <crt_externs.h>
+# define C_getenventry(i)       ((*_NSGetEnviron())[ i ])
+#elif defined(C_MACOSX)
+# define C_getenventry(i)       NULL
+#else
+extern char **environ;
+# define C_getenventry(i)       (environ[ i ])
+#endif
+
+#ifndef ENV_MAX
+# define ENV_MAX        1024
+#endif
+
+#ifndef FILENAME_MAX
+# define FILENAME_MAX          1024
+#endif
+
+static C_TLS char *C_exec_args[ ARG_MAX ];
+static C_TLS char *C_exec_env[ ENV_MAX ];
+static C_TLS struct utsname C_utsname;
+static C_TLS struct flock C_flock;
+static C_TLS DIR *temphandle;
+static C_TLS struct passwd *C_user;
+#ifdef HAVE_GRP_H
+static C_TLS struct group *C_group;
+#else
+static C_TLS struct {
+    char *gr_name, gr_passwd;
+    int gr_gid;
+    char *gr_mem[ 1 ];
+} C_group = { "", "", 0, { "" } };
+#endif
+
+# define C_PW_GECOS (C_user->pw_gecos)
+
+static C_TLS int C_pipefds[ 2 ];
+static C_TLS time_t C_secs;
+static C_TLS struct timeval C_timeval;
+static C_TLS char C_hostbuf[ 256 ];
+static C_TLS struct stat C_statbuf;
+
+#define C_mkdir(str)        C_fix(mkdir(C_c_string(str), S_IRWXU | S_IRWXG | S_IRWXO))
+#define C_fchdir(fd)        C_fix(fchdir(C_unfix(fd)))
+#define C_chdir(str)        C_fix(chdir(C_c_string(str)))
+#define C_rmdir(str)        C_fix(rmdir(C_c_string(str)))
+
+#define open_binary_input_pipe(a, n, name)   C_mpointer(a, popen(C_c_string(name), "r"))
+#define open_text_input_pipe(a, n, name)     open_binary_input_pipe(a, n, name)
+#define open_binary_output_pipe(a, n, name)  C_mpointer(a, popen(C_c_string(name), "w"))
+#define open_text_output_pipe(a, n, name)    open_binary_output_pipe(a, n, name)
+#define close_pipe(p)                        C_fix(pclose(C_port_file(p)))
+
+#define C_fork              fork
+#define C_waitpid(id, o)    C_fix(waitpid(C_unfix(id), &C_wait_status, C_unfix(o)))
+#define C_getppid           getppid
+#define C_kill(id, s)       C_fix(kill(C_unfix(id), C_unfix(s)))
+#define C_getuid            getuid
+#define C_getgid            getgid
+#define C_geteuid           geteuid
+#define C_getegid           getegid
+#define C_chown(fn, u, g)   C_fix(chown(C_data_pointer(fn), C_unfix(u), C_unfix(g)))
+#define C_chmod(fn, m)      C_fix(chmod(C_data_pointer(fn), C_unfix(m)))
+#define C_setuid(id)        C_fix(setuid(C_unfix(id)))
+#define C_setgid(id)        C_fix(setgid(C_unfix(id)))
+#define C_seteuid(id)       C_fix(seteuid(C_unfix(id)))
+#define C_setegid(id)       C_fix(setegid(C_unfix(id)))
+#define C_setsid(dummy)     C_fix(setsid())
+#define C_setpgid(x, y)     C_fix(setpgid(C_unfix(x), C_unfix(y)))
+#define C_getpgid(x)        C_fix(getpgid(C_unfix(x)))
+#define C_symlink(o, n)     C_fix(symlink(C_data_pointer(o), C_data_pointer(n)))
+#define C_do_readlink(f, b)    C_fix(readlink(C_data_pointer(f), C_data_pointer(b), FILENAME_MAX))
+#define C_getpwnam(n)       C_mk_bool((C_user = getpwnam((char *)C_data_pointer(n))) != NULL)
+#define C_getpwuid(u)       C_mk_bool((C_user = getpwuid(C_unfix(u))) != NULL)
+#if defined(HAVE_GRP_H)
+#define C_getgrnam(n)       C_mk_bool((C_group = getgrnam((char *)C_data_pointer(n))) != NULL)
+#define C_getgrgid(u)       C_mk_bool((C_group = getgrgid(C_unfix(u))) != NULL)
+#else
+#define C_getgrnam(n)       C_SCHEME_FALSE
+#define C_getgrgid(n)       C_SCHEME_FALSE
+#endif
+#define C_pipe(d)           C_fix(pipe(C_pipefds))
+#define C_truncate(f, n)    C_fix(truncate((char *)C_data_pointer(f), C_num_to_int(n)))
+#define C_ftruncate(f, n)   C_fix(ftruncate(C_unfix(f), C_num_to_int(n)))
+#define C_uname             C_fix(uname(&C_utsname))
+#define C_alarm             alarm
+#define C_setvbuf(p, m, s)  C_fix(setvbuf(C_port_file(p), NULL, C_unfix(m), C_unfix(s)))
+#define C_test_access(fn, m)     C_fix(access((char *)C_data_pointer(fn), C_unfix(m)))
+#define C_close(fd)         C_fix(close(C_unfix(fd)))
+#define C_sleep             sleep
+#define C_umask(m)          C_fix(umask(C_unfix(m)))
+
+#define C_lstat(fn)         C_fix(lstat((char *)C_data_pointer(fn), &C_statbuf))
+
+static void C_fcall C_set_arg_string(char **where, int i, char *a, int len) {
+    char *ptr;
+    if(a != NULL) {
+        ptr = (char *)C_malloc(len + 1);
+        C_memcpy(ptr, a, len);
+        ptr[ len ] = '\0';
+        /* Can't barf() here, so the NUL byte check happens in Scheme */
+    }
+    else ptr = NULL;
+    where[ i ] = ptr;
+}
+
+static void C_fcall C_free_arg_string(char **where) {
+    while((*where) != NULL) C_free(*(where++));
+}
+
+#define C_set_exec_arg(i, a, len)	C_set_arg_string(C_exec_args, i, a, len)
+#define C_free_exec_args()		C_free_arg_string(C_exec_args)
+#define C_set_exec_env(i, a, len)	C_set_arg_string(C_exec_env, i, a, len)
+#define C_free_exec_env()		C_free_arg_string(C_exec_env)
+
+#define C_execvp(f)         C_fix(execvp(C_data_pointer(f), C_exec_args))
+#define C_execve(f)         C_fix(execve(C_data_pointer(f), C_exec_args, C_exec_env))
+
+#if defined(C_MACOSX)
+static C_TLS int C_uw;
+# define C_WIFEXITED(n)      (C_uw = C_unfix(n), C_mk_bool(WIFEXITED(C_uw)))
+# define C_WIFSIGNALED(n)    (C_uw = C_unfix(n), C_mk_bool(WIFSIGNALED(C_uw)))
+# define C_WIFSTOPPED(n)     (C_uw = C_unfix(n), C_mk_bool(WIFSTOPPED(C_uw)))
+# define C_WEXITSTATUS(n)    (C_uw = C_unfix(n), C_fix(WEXITSTATUS(C_uw)))
+# define C_WTERMSIG(n)       (C_uw = C_unfix(n), C_fix(WTERMSIG(C_uw)))
+# define C_WSTOPSIG(n)       (C_uw = C_unfix(n), C_fix(WSTOPSIG(C_uw)))
+#else
+# define C_WIFEXITED(n)      C_mk_bool(WIFEXITED(C_unfix(n)))
+# define C_WIFSIGNALED(n)    C_mk_bool(WIFSIGNALED(C_unfix(n)))
+# define C_WIFSTOPPED(n)     C_mk_bool(WIFSTOPPED(C_unfix(n)))
+# define C_WEXITSTATUS(n)    C_fix(WEXITSTATUS(C_unfix(n)))
+# define C_WTERMSIG(n)       C_fix(WTERMSIG(C_unfix(n)))
+# define C_WSTOPSIG(n)       C_fix(WSTOPSIG(C_unfix(n)))
+#endif
+
+#define C_mkfifo(fn, m)    C_fix(mkfifo((char *)C_data_pointer(fn), C_unfix(m)))
+
+#define C_flock_setup(t, s, n) (C_flock.l_type = C_unfix(t), C_flock.l_start = C_num_to_int(s), C_flock.l_whence = SEEK_SET, C_flock.l_len = C_num_to_int(n), C_SCHEME_UNDEFINED)
+#define C_flock_test(p)     (fcntl(fileno(C_port_file(p)), F_GETLK, &C_flock) >= 0 ? (C_flock.l_type == F_UNLCK ? C_fix(0) : C_fix(C_flock.l_pid)) : C_SCHEME_FALSE)
+#define C_flock_lock(p)     C_fix(fcntl(fileno(C_port_file(p)), F_SETLK, &C_flock))
+#define C_flock_lockw(p)    C_fix(fcntl(fileno(C_port_file(p)), F_SETLKW, &C_flock))
+
+static C_TLS sigset_t C_sigset;
+#define C_sigemptyset(d)    (sigemptyset(&C_sigset), C_SCHEME_UNDEFINED)
+#define C_sigaddset(s)      (sigaddset(&C_sigset, C_unfix(s)), C_SCHEME_UNDEFINED)
+#define C_sigdelset(s)      (sigdelset(&C_sigset, C_unfix(s)), C_SCHEME_UNDEFINED)
+#define C_sigismember(s)    C_mk_bool(sigismember(&C_sigset, C_unfix(s)))
+#define C_sigprocmask_set(d)        C_fix(sigprocmask(SIG_SETMASK, &C_sigset, NULL))
+#define C_sigprocmask_block(d)      C_fix(sigprocmask(SIG_BLOCK, &C_sigset, NULL))
+#define C_sigprocmask_unblock(d)    C_fix(sigprocmask(SIG_UNBLOCK, &C_sigset, NULL))
+#define C_sigprocmask_get(d)        C_fix(sigprocmask(SIG_SETMASK, NULL, &C_sigset))
+
+#define C_open(fn, fl, m)   C_fix(open(C_c_string(fn), C_unfix(fl), C_unfix(m)))
+#define C_read(fd, b, n)    C_fix(read(C_unfix(fd), C_data_pointer(b), C_unfix(n)))
+#define C_write(fd, b, n)   C_fix(write(C_unfix(fd), C_data_pointer(b), C_unfix(n)))
+#define C_mkstemp(t)        C_fix(mkstemp(C_c_string(t)))
+
+#define C_ctime(n)          (C_secs = (n), ctime(&C_secs))
+
+#if defined(C_MACOSX)
+/* Seen here: http://lists.samba.org/archive/samba-technical/2002-November/025571.html */
+
+static time_t C_timegm(struct tm *t)
+{
+    time_t tl, tb;
+    struct tm *tg;
+
+    tl = mktime (t);
+    if (tl == -1)
+    {
+        t->tm_hour--;
+        tl = mktime (t);
+        if (tl == -1)
+            return -1; /* can't deal with output from strptime */
+        tl += 3600;
+    }
+    tg = gmtime (&tl);
+    tg->tm_isdst = 0;
+    tb = mktime (tg);
+    if (tb == -1)
+    {
+        tg->tm_hour--;
+        tb = mktime (tg);
+        if (tb == -1)
+            return -1; /* can't deal with output from gmtime */
+        tb += 3600;
+    }
+    return (tl - (tb - tl));
+}
+#else
+#define C_timegm timegm
+#endif
+
+#define C_a_timegm(ptr, c, v, tm)  C_flonum(ptr, C_timegm(C_tm_set((v), C_data_pointer(tm))))
+
+#ifdef __linux__
+extern char *strptime(const char *s, const char *format, struct tm *tm);
+extern pid_t getpgid(pid_t pid);
+#endif
+
+/* tm_get could be in posix-common, but it's only used in here */
+#define cpy_tmstc08_to_tmvec(v, ptm) \
+    (C_set_block_item((v), 0, C_fix(((struct tm *)ptm)->tm_sec)), \
+     C_set_block_item((v), 1, C_fix((ptm)->tm_min)), \
+     C_set_block_item((v), 2, C_fix((ptm)->tm_hour)), \
+     C_set_block_item((v), 3, C_fix((ptm)->tm_mday)), \
+     C_set_block_item((v), 4, C_fix((ptm)->tm_mon)), \
+     C_set_block_item((v), 5, C_fix((ptm)->tm_year)), \
+     C_set_block_item((v), 6, C_fix((ptm)->tm_wday)), \
+     C_set_block_item((v), 7, C_fix((ptm)->tm_yday)), \
+     C_set_block_item((v), 8, ((ptm)->tm_isdst ? C_SCHEME_TRUE : C_SCHEME_FALSE)))
+
+#define cpy_tmstc9_to_tmvec(v, ptm) \
+    (C_set_block_item((v), 9, C_fix(-(ptm)->tm_gmtoff)))
+
+#define C_tm_get_08(v, tm)  cpy_tmstc08_to_tmvec( (v), (tm) )
+#define C_tm_get_9(v, tm)   cpy_tmstc9_to_tmvec( (v), (tm) )
+
+    static C_word
+C_tm_get( C_word v, void *tm )
+{
+    C_tm_get_08( v, (struct tm *)tm );
+#if defined(C_GNU_ENV) && !defined(__uClinux__)
+    C_tm_get_9( v, (struct tm *)tm );
+#endif
+    return v;
+}
+
+#define C_strptime(s, f, v, stm) \
+    (strptime(C_c_string(s), C_c_string(f), ((struct tm *)(stm))) ? C_tm_get((v), (stm)) : C_SCHEME_FALSE)
+
+static gid_t *C_groups = NULL;
+
+#define C_get_gid(n)      C_fix(C_groups[ C_unfix(n) ])
+#define C_set_gid(n, id)  (C_groups[ C_unfix(n) ] = C_unfix(id), C_SCHEME_UNDEFINED)
+#define C_set_groups(n)   C_fix(setgroups(C_unfix(n), C_groups))
+
+#if defined(TIOCGWINSZ)
+static int get_tty_size(int p, int *rows, int *cols)
+{
+    struct winsize tty_size;
+    int r;
+
+    memset(&tty_size, 0, sizeof tty_size);
+
+    r = ioctl(p, TIOCGWINSZ, &tty_size);
+    if (r == 0) {
+        *rows = tty_size.ws_row;
+        *cols = tty_size.ws_col;
+    }
+    return r;
+}
+#else
+static int get_tty_size(int p, int *rows, int *cols)
+{
+    *rows = *cols = 0;
+    return -1;
+}
+#endif
+
+static int set_file_mtime(char *filename, C_word tm)
+{
+    struct utimbuf tb;
+
+    tb.actime = tb.modtime = C_num_to_int(tm);
+    return utime(filename, &tb);
+}
+
+static C_word C_i_fifo_p(C_word name) 
+{
+    struct stat buf;
+    int res;
+
+    res = stat(C_c_string(name), &buf);
+
+    if(res != 0) {
+        if(errno == ENOENT) return C_fix(0);
+        else return C_fix(res);
+    }
+
+    if((buf.st_mode & S_IFMT) == S_IFIFO) return C_SCHEME_TRUE;
+    else return C_SCHEME_FALSE;
+}
+
+#endif
