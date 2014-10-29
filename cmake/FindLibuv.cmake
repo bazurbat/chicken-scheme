04e47df1a4a7c5b3fdb132e2b562b22333f61da1
Oleg Kolosov <bazurbat@gmail.com>
2014-10-30 01:24:56 +0400

    Slight adjustments to FindLibuv to ease building on Windows

--- cmake/FindLibuv.cmake
+++ cmake/FindLibuv.cmake
@@ -10,15 +10,18 @@ if(LIBUV_ROOT_DIR)
     find_path(LIBUV_INCLUDE_DIRS uv.h
         PATHS ${LIBUV_ROOT_DIR}/include
         NO_DEFAULT_PATH)
-    find_library(LIBUV_LIBRARIES uv
-        PATHS ${LIBUV_ROOT_DIR}/lib
-            ${LIBUV_ROOT_DIR}/lib64
-            ${LIBUV_ROOT_DIR}/lib32
+    find_library(LIBUV_LIBRARIES
+    	NAMES uv libuv
+        PATHS ${LIBUV_ROOT_DIR}
+        	  ${LIBUV_ROOT_DIR}/lib
+              ${LIBUV_ROOT_DIR}/lib64
+              ${LIBUV_ROOT_DIR}/lib32
         NO_DEFAULT_PATH)
 endif()
 
 find_path(LIBUV_INCLUDE_DIRS uv.h)
-find_library(LIBUV_LIBRARIES uv)
+find_library(LIBUV_LIBRARIES
+	NAMES uv libuv)
 
 find_package_handle_standard_args(Libuv DEFAULT_MSG
     LIBUV_LIBRARIES LIBUV_INCLUDE_DIRS)
