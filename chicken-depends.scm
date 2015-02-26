30422e88f580dd981eb15becd404d60d71e3f955
Oleg Kolosov <bazurbat@gmail.com>
2015-02-27 00:06:09 +0400

    Add CMake build system support

---
+++ chicken-depends.scm
@@ -0,0 +1,121 @@
+(import scheme chicken)
+(use data-structures files posix extras)
+
+(define (remove pred ls)
+  (cond ((null? ls) '())
+        ((pred (car ls)) (remove pred (cdr ls)))
+        (else (cons (car ls) (remove pred (cdr ls))))))
+
+(define library-modules '(srfi-13 srfi-14 srfi-18 srfi-69))
+
+(define core-modules
+  (remove (cut memq <> library-modules)
+          `(list scheme chicken foreign
+                 ,@##sys#core-library-modules
+                 ,@##sys#core-syntax-modules)))
+
+(define modules '())
+(define imports '())
+(define includes '())
+
+; (register-feature! 'debug)
+
+(cond-expand
+  (debug
+    (define-syntax d
+      (syntax-rules ()
+        ((_ arg args ...)
+         (print arg args ...)))))
+  (else
+    (define-syntax d
+      (syntax-rules () ((_ . _) (void))))))
+
+;; copied from chicken/support.scm
+
+(set! ##sys#user-read-hook
+  (let ([old-hook ##sys#user-read-hook])
+    (lambda (char port)
+      (if (char=? #\> char)
+        (let* ((_ (read-char port))           ; swallow #\>
+               (text (scan-sharp-greater-string port)))
+          `(declare (foreign-declare ,text)) )
+        (old-hook char port) ) ) ) )
+
+(define (scan-sharp-greater-string port)
+  (let ([out (open-output-string)])
+    (let loop ()
+      (let ([c (read-char port)])
+        (cond [(eof-object? c) (quit "unexpected end of `#> ... <#' sequence")]
+              [(char=? c #\newline)
+               (newline out)
+               (loop) ]
+              [(char=? c #\<)
+               (let ([c (read-char port)])
+                 (if (eqv? #\# c)
+                   (get-output-string out)
+                   (begin
+                     (write-char #\< out)
+                     (write-char c out)
+                     (loop) ) ) ) ]
+              [else
+                (write-char c out)
+                (loop) ] ) ) ) ) )
+
+(define (extract-names expr)
+  (let loop ((expr expr) (names '()))
+    (cond ((symbol? expr)
+           (d "symbol " expr (if (memq expr core-modules) " (core)" ""))
+           (if (memq expr core-modules) names (cons expr names)))
+          ((pair? expr)
+           (case (car expr)
+             ((srfi)
+              (map (compose (cut string-append "srfi-" <>) number->string)
+                   (cdr expr)))
+             ((only except prefix rename)
+              (loop (cadr expr) names)))))))
+
+(define (get-imports ls)
+  (when (pair? ls)
+    (let ((first (car ls))
+          (rest  (cdr ls)))
+      (case first
+        ((quote quasiquote))
+        ((import use requre require-extension require-library)
+         (set! imports (append imports
+                               (apply append (map extract-names rest))))
+         (d "imports: " imports))
+        ((module)
+         (set! modules (cons (car rest) modules))
+         (get-imports (cdr rest)))
+        ((include)
+         (set! includes (cons (car rest) includes)))
+        (else
+          (get-imports first)
+          (get-imports rest))))))
+
+(define (extract-imports)
+  (let ((e (read)))
+    (unless (eof-object? e)
+      (get-imports e)
+      (extract-imports))))
+
+(define (main args)
+  (handle-exceptions
+    exn (print-error-message exn (current-error-port))
+    (define in-file (car args))
+    (define out-file (cadr args))
+    (define (write-imports)
+      (with-input-from-file in-file extract-imports)
+      (write-string "set(imports ")
+      (write-string (string-intersperse (map ->string imports)))
+      (write-string ")")(newline)
+      (write-string "set(includes ")
+      (write-string (string-intersperse (map ->string includes)))
+      (write-string ")")(newline))
+  (and (file-exists? out-file) (delete-file out-file))
+  (create-directory (pathname-directory out-file) #t)
+  (with-output-to-file out-file write-imports)))
+
+(cond-expand
+  (chicken-script)
+  (else (main (command-line-arguments))))
