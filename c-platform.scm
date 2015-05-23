f40f5daf969459b2601dfec39dfdcfcf82c2ccc9
Peter Bex <peter@more-magic.net>
2015-05-23 16:14:36 +0200

    Fix size calculation for generated code for (list ...).
    The calculation was off by one, which might cause errors.
    
    Signed-off-by: Evan Hanson <evhan@foldling.org>

--- c-platform.scm
+++ c-platform.scm
@@ -838,8 +838,8 @@
 
 (rewrite 'cons 16 2 "C_a_i_cons" #t 3)
 (rewrite '##sys#cons 16 2 "C_a_i_cons" #t 3)
-(rewrite 'list 16 #f "C_a_i_list" #t '(3) #t)
-(rewrite '##sys#list 16 #f "C_a_i_list" #t '(3))
+(rewrite 'list 16 #f "C_a_i_list" #t '(1 3) #t)
+(rewrite '##sys#list 16 #f "C_a_i_list" #t '(1 3))
 (rewrite 'vector 16 #f "C_a_i_vector" #t #t #t)
 (rewrite '##sys#vector 16 #f "C_a_i_vector" #t #t)
 (rewrite '##sys#make-structure 16 #f "C_a_i_record" #t #t #t)
--- optimizer.scm
+++ optimizer.scm
@@ -1170,8 +1170,9 @@
     ;; (<alloc-op> ...) -> (##core#inline_allocate (<aiop> <words>) ...)
     ((16) ; classargs = (<argc> <aiop> <safe> <words> [<counted>])
      ;; - <argc> may be #f, saying that any number of arguments is allowed,
-     ;; - <words> may be a list of one element (the number of words), meaning that
-     ;;   the words are to be multiplied with the number of arguments.
+     ;; - <words> may be a list of two elements (the base number of words and
+     ;;   the number of words per element), meaning that the words are to be
+     ;;   multiplied with the number of arguments.
      ;; - <words> may also be #t, meaning that the number of words is the same as the
      ;;   number of arguments plus 1.
      ;; - if <counted> is given and true and <argc> is between 1-8, append "<count>"
@@ -1194,7 +1195,8 @@
 			      (conc (second classargs) rargc)
 			      (second classargs) )
 			  (cond ((eq? #t w) (add1 rargc))
-				((pair? w) (* rargc (car w)))
+				((pair? w) (+ (car w)
+					      (* rargc (cadr w))))
 				(else w) ) )
 		    callargs) ) ) ) ) )
 
