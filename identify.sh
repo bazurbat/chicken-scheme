4e1f7272a3536dd93577c71f0a8bad16dab2a308
Benutzer <Joerg.Wittenberger@softeyes.net>
2015-06-13 14:14:01 +0200

    Modify identify.sh to make custom builds easier.
    Signed-off-by: Peter Bex <peter@more-magic.net>
    Signed-off-by: Mario Domenech Goulart <mario.goulart@gmail.com>

--- identify.sh
+++ identify.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 #
 # identify.sh - check for .git directory and obtain checked out revision and branch
 #
@@ -21,20 +21,23 @@ if test -d "$1/.git"; then
     rev=`GIT_DIR="$1/.git" git rev-parse --short HEAD 2>/dev/null`
     branchname=`GIT_DIR="$1/.git" git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
     tag="#define C_BUILD_TAG \"compiled ${buildtime} on ${host} (${usys})\""
+else
+    branchname=${branchname:-"custom"}
+    tag=${tag:-"#define C_BUILD_TAG \"compiled ${buildtime} on ${host} (${usys})\""}
+fi
 
-    case "${branchname}" in
-	"") branchname="";;
-        "(no branch)") branchname="";;
-	"master") branchname="";;
-	"release") branchname="";;
-    esac
-    if test "x${rev0}" \!= "x${rev}"; then
-	echo ${rev} >buildid
-    fi
-    if test "x${branchname0}" \!= "x${branchname}"; then
-	echo ${branchname} >buildbranch
-    fi
-    if test "x${tag0}" \!= "x${tag}"; then
-	echo ${tag} >buildtag.h
-    fi
+case "${branchname}" in
+    "") branchname="";;
+    "(no branch)") branchname="";;
+    "master") branchname="";;
+    "release") branchname="";;
+esac
+if test "x${rev0}" \!= "x${rev}"; then
+    echo ${rev} >buildid
+fi
+if test "x${branchname0}" \!= "x${branchname}"; then
+    echo ${branchname} >buildbranch
+fi
+if test "x${tag0}" \!= "x${tag}"; then
+    echo ${tag} >buildtag.h
 fi
