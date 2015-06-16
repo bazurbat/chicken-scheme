bazurbat/chicken-scheme

commit 25db851b902606741b1a520bd7e4a3fbd12c9b2a

Fix buffer overrun in substring-index[-ci]
When passing a start index greater than 0, substring-index[-ci] would
scan past the end of the subject string, leading to bogus results in
case the substring is accidentally run into beyond the end of the
subject. This patch fixes the issue and also adds a range check for the
start index.

Signed-off-by: Peter Bex <peter.bex@xs4all.nl>

data-structures.scm
@@ -307,15 +307,21 @@
   (define (traverse which where start test loc)
     (##sys#check-string which loc)
     (##sys#check-string where loc)
-    (let ([wherelen (##sys#size where)]
-	  [whichlen (##sys#size which)] )
+    (let* ((wherelen (##sys#size where))
+	   (whichlen (##sys#size which))
+	   (end (fx- wherelen whichlen)))
       (##sys#check-exact start loc)
-      (let loop ([istart start] [iend whichlen])
-	(cond [(fx> iend wherelen) #f]
-	      [(test istart whichlen) istart]
-	      [else 
-	       (loop (fx+ istart 1)
-		     (fx+ iend 1) ) ] ) ) ) )
+      (if (and (fx>= start 0)
+	       (fx> wherelen start))
+	  (let loop ((istart start))
+	    (cond ((fx> istart end) #f)
+		  ((test istart whichlen) istart)
+		  (else (loop (fx+ istart 1)))))
+	  (##sys#error-hook (foreign-value "C_OUT_OF_RANGE_ERROR" int)
+			    loc
+			    start
+			    wherelen))))
+
   (set! ##sys#substring-index 
     (lambda (which where start)
       (traverse 

commit e6723ded1f21a814f318bf8bf26a73fad15e3e6c

data-structures: fix substring-index[-ci] corner case ("" as 2nd arg)
Fix regression introduced by 25db851b90260:

$ ~/local/chicken-4.9.0.1/bin/csi -p '(substring-index "foo" "")'

$ ~/local/chicken-4.10.0rc1/bin/csi -p '(substring-index "foo" "")'

Error: (substring-index) out of range
0
0

        Call history:

        <syntax>          (substring-index "foo" "")
        <eval>    (substring-index "foo" "")    <--

Signed-off-by: Mario Domenech Goulart <mario.goulart@gmail.com>

data-structures.scm
@@ -312,11 +312,14 @@
 	   (end (fx- wherelen whichlen)))
       (##sys#check-exact start loc)
       (if (and (fx>= start 0)
-	       (fx> wherelen start))
-	  (let loop ((istart start))
-	    (cond ((fx> istart end) #f)
-		  ((test istart whichlen) istart)
-		  (else (loop (fx+ istart 1)))))
+	       (fx>= wherelen start))
+	  (if (fx= whichlen 0)
+	      start
+	      (and (fx>= end 0)
+		   (let loop ((istart start))
+		     (cond ((fx> istart end) #f)
+			   ((test istart whichlen) istart)
+			   (else (loop (fx+ istart 1)))))))
 	  (##sys#error-hook (foreign-value "C_OUT_OF_RANGE_ERROR" int)
 			    loc
 			    start

tests/data-structures-tests.scm
@@ -47,6 +47,12 @@
 (assert (not (substring-ci=? "foo\x00a" "foo\x00b" 1 1)))
 (assert (not (substring-index "o\x00bar" "foo\x00baz")))
 (assert (not (substring-index-ci "o\x00bar" "foo\x00baz")))
+(assert (= 0 (substring-index "" "")))
+(assert (= 1 (substring-index "" "a" 1)))
+(assert-error (substring-index "" "a" 2))
+(assert-error (substring-index "a" "b" 2))
+(assert (not (substring-index "a" "b" 1)))
+(assert (not (substring-index "ab" "")))
 (assert (= 0 (string-compare3 "foo\x00a" "foo\x00a")))
 (assert (> 0 (string-compare3 "foo\x00a" "foo\x00b")))
 (assert (< 0 (string-compare3 "foo\x00b" "foo\x00a")))