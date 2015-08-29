bazurbat/chicken-scheme

commit 5639ad58ce7d30a87970e561e782d13e088a1064

Modify identify.sh to make custom builds easier.
Signed-off-by: Peter Bex <peter@more-magic.net>
Signed-off-by: Mario Domenech Goulart <mario.goulart@gmail.com>

identify.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 #
 # identify.sh - check for .git directory and obtain checked out revision and branch
 #
@@ -21,20 +21,23 @@ if test -d "$1/.git"; then
     rev=`GIT_DIR="$1/.git" git rev-parse --short HEAD 2>/dev/null`
     branchname=`GIT_DIR="$1/.git" git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
     tag="#define C_BUILD_TAG \"compiled ${buildtime} on ${host} (${usys})\""
+else
+    branchname=${branchname:-"custom"}
+    tag=${tag:-"#define C_BUILD_TAG \"compiled ${buildtime} on ${host} (${usys})\""}
+fi
 
-    case "${branchname}" in
-	"") branchname="";;
-        "(no branch)") branchname="";;
-	"master") branchname="";;
-	"release") branchname="";;
-    esac
-    if test "x${rev0}" \!= "x${rev}"; then
-	echo ${rev} >buildid
-    fi
-    if test "x${branchname0}" \!= "x${branchname}"; then
-	echo ${branchname} >buildbranch
-    fi
-    if test "x${tag0}" \!= "x${tag}"; then
-	echo ${tag} >buildtag.h
-    fi
+case "${branchname}" in
+    "") branchname="";;
+    "(no branch)") branchname="";;
+    "master") branchname="";;
+    "release") branchname="";;
+esac
+if test "x${rev0}" \!= "x${rev}"; then
+    echo ${rev} >buildid
+fi
+if test "x${branchname0}" \!= "x${branchname}"; then
+    echo ${branchname} >buildbranch
+fi
+if test "x${tag0}" \!= "x${tag}"; then
+    echo ${tag} >buildtag.h
 fi

commit eb1e95e041f617a7076cd2dda74040cba0941ca0

Fix "identify.sh" for release and snapshot builds.
This is a bit of a hack: check whether the manual-html directory
exists and assume it must be a distribution build, then exit to
avoid a rebuild which requires a pre-existing chicken.

Changeset 5639ad58ce7d30a87970e561e782d13e088a1064 (for "allowing
custom builds") caused this breakage.

identify.sh
@@ -21,6 +21,10 @@ if test -d "$1/.git"; then
     rev=`GIT_DIR="$1/.git" git rev-parse --short HEAD 2>/dev/null`
     branchname=`GIT_DIR="$1/.git" git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
     tag="#define C_BUILD_TAG \"compiled ${buildtime} on ${host} (${usys})\""
+elif test -d "$1/manual-html"; then
+    # Assume this is a snapshot or release build; don't touch
+    # identifying files to avoid a rebuild which needs chicken
+    exit
 else
     branchname=${branchname:-"custom"}
     tag=${tag:-"#define C_BUILD_TAG \"compiled ${buildtime} on ${host} (${usys})\""}