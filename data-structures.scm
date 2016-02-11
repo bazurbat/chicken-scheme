df99ec3e9b5f5be334a179e57b64984f29af8817
Peter Bex <peter@more-magic.net>
2016-02-11 21:20:00 +0100

    Fix type signatures of a few alist procedures
    Thanks to Joerg Wittenberger for pointing out that the types weren't
    exactly right.
    
    This also makes the types and higher order procedures consistent in how
    they call their argument predicates: always the supplied key first, and
    the "thing in the list" second.
    
    Signed-off-by: Evan Hanson <evhan@foldling.org>

--- data-structures.scm
+++ data-structures.scm
@@ -206,7 +206,7 @@
 		      (let loop ([lst lst])
 			(and (pair? lst)
 			     (let ([a (##sys#slot lst 0)])
-			       (if (and (pair? a) (cmp (##sys#slot a 0) x))
+			       (if (and (pair? a) (cmp x (##sys#slot a 0)))
 				   a
 				   (loop (##sys#slot lst 1)) ) ) ) ) ) ] ) ] 
 	 [item (aq x lst)] )
@@ -226,7 +226,7 @@
            (let ((a (##sys#slot lst 0)))
              (cond ((not (pair? a))
                     (error 'alist-update "bad argument type" a))
-                   ((cmp (##sys#slot a 0) k)
+                   ((cmp k (##sys#slot a 0))
                     (cons (cons k v) (##sys#slot lst 1)))
                    (else
                     (cons (cons (##sys#slot a 0) (##sys#slot a 1))
@@ -244,7 +244,7 @@
 			 ((pair? lst)
 			  (let ((a (##sys#slot lst 0)))
 			    (##sys#check-pair a 'alist-ref)
-			    (if (cmp (##sys#slot a 0) x)
+			    (if (cmp x (##sys#slot a 0))
 				a
 				(loop (##sys#slot lst 1)) ) ))
 			 (else (error 'alist-ref "bad argument type" lst)) )  ) ) ) ) )
